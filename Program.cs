e06b77426029786b98696c29c94ecd385cf7b577
tramlun <khanhtram1812@gmail.com>
2021-08-02 21:12:37 +0700

    Upload code ne
    Tram up code len nhe

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CS001_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
