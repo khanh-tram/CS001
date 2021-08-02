8647f430f092cc0a98b0a8ece90f037ed04c8a9c
tramlun <khanhtram1812@gmail.com>
2021-08-02 21:32:36 +0700

    ver8
    tui dep

--- Program.cs
+++ Program.cs
@@ -15,6 +15,8 @@ namespace CS001_HelloWorld
             Console.WriteLine("2");
             
             Console.WriteLine("4");
+
+            Console.WriteLine("local");
             
             Console.WriteLine("server");
             
